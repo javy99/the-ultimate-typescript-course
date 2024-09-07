7269c6c7e6ad1938e916cd55ce50b3b8133f5693
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-07 22:29:40 +0200

    Organizing into folders

--- 2. Fundamentals/dist/index.js
+++ 2. Fundamentals/dist/index.js
@@ -19,4 +19,18 @@ const medium = 2;
 const large = 3;
 let mySize = 2;
 console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000, 2022);
+calculateTax(10000);
+let employee = {
+    id: 1,
+    name: 'Javy',
+    retire: (date) => {
+        console.log('Retiring on ' + date);
+    }
+};
 //# sourceMappingURL=index.js.map
