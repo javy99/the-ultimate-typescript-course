db9395820bf8fbfefd986fc1be796b5130f9e6d2
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-08 11:44:15 +0200

    Inheritance

--- 4. Classes, Interfaces and Object-oriented Programming/src/index.ts
+++ 4. Classes, Interfaces and Object-oriented Programming/src/index.ts
@@ -169,3 +169,28 @@ let ride2 = new Ride();
 ride2.start();
 
 console.log(Ride.activeRides);
+
+// Inheritance
+class Person {
+    constructor(public firstName: string, public lastName: string) { }
+
+    get fullName() {
+        return this.firstName + ' ' + this.lastName;
+    }
+
+    walk() {
+        console.log('Walking...');
+    }
+}
+
+class Student extends Person {
+    constructor(public studentId: number, firstName: string, lastName: string) {
+        super(firstName, lastName);
+    }
+
+    takeTest() {
+        console.log('Taking a test...');
+    }
+}
+
+const student = new Student(1, 'Javy', 'Kosimov');
