c37bd7f6347e45a7eddc80180890119640c81d79
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-08 10:55:53 +0200

    Read-Only and Optional Properties

--- 4. Classes, Interfaces and Object-oriented Programming/src/index.ts
+++ 4. Classes, Interfaces and Object-oriented Programming/src/index.ts
@@ -25,3 +25,23 @@ console.log(account.balance); // 1100
 console.log(account); // Account { id: 1, owner: 'Javy', balance: 1100 }
 console.log(typeof account); // object
 console.log(account instanceof Account); // true
+
+// Read-Only and Optional Properties
+class Account1 {
+    readonly id: number;
+    owner: string;
+    balance: number;
+    nickname?: string;
+
+    constructor(id: number, owner: string, balance: number) {
+        this.id = id;
+        this.owner = owner
+        this.balance = balance;
+    }
+
+    deposit(amount: number): void {
+        if (amount <= 0)
+            throw new Error('Invalid amount');
+        this.balance += amount;
+    }
+}
