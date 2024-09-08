9dc4b8445020acd4758a1191dd78abe704dbb926
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-08 11:20:32 +0200

    Access Control Keywords

--- 4. Classes, Interfaces and Object-oriented Programming/src/index.ts
+++ 4. Classes, Interfaces and Object-oriented Programming/src/index.ts
@@ -45,3 +45,33 @@ class Account1 {
         this.balance += amount;
     }
 }
+
+// Access Control Keywords
+class Account2 {
+    readonly id: number;
+    owner: string;
+    private _balance: number;
+    nickname?: string;
+
+    constructor(id: number, owner: string, balance: number) {
+        this.id = id;
+        this.owner = owner
+        this._balance = balance;
+    }
+
+    deposit(amount: number): void {
+        if (amount <= 0)
+            throw new Error('Invalid amount');
+        this._balance += amount;
+    }
+
+    private calculateTax() {
+    }
+
+    getBalance(): number {
+        return this._balance;
+    }
+}
+
+let account2 = new Account2(1, 'Javy', 1000);
+console.log(account2.getBalance());
