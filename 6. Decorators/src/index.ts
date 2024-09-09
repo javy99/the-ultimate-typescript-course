bc67474a90d2121d3dd36deaf34485e1a876f19a
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-09 22:24:06 +0200

    Parameterized Decorators

--- 6. Decorators/src/index.ts
+++ 6. Decorators/src/index.ts
@@ -18,3 +18,23 @@ function Component(constructor: Function) {
 
 @Component
 class ProfileComponent { }
+
+// Parameterized Decorators
+type ComponentOptions = {
+    selector: string;
+}
+
+// Decorator Factory
+function Component1(options: ComponentOptions) {
+    return (constructor: Function) => {
+        console.log('Component decorator called');
+        constructor.prototype.options = options;
+        constructor.prototype.uniqueId = Date.now();
+        constructor.prototype.insertInDOM = () => {
+            console.log('Inserting the component in the DOM');
+        }
+    }
+}
+
+@Component1({ selector: '#my-profile' })
+class ProfileComponent1 { }
