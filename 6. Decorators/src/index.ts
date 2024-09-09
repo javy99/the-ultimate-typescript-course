3684e909786baead5fe1d49850049fbe5abf20a3
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-09 22:19:09 +0200

    Class Decorators

--- 6. Decorators/src/index.ts
+++ 6. Decorators/src/index.ts
@@ -1,5 +1,20 @@
 // What are Decorators?
 // Decorators - are attributes that we apply to our classes and their members and with this we can change how they behave. They are frequently used in Angular and vue Applications.
-class ProfileComponent {
 
+// Class Decorators
+// class Component {
+//     insertInDOM() { }
+// }
+
+// class ProfileComponent extends Component { }
+
+function Component(constructor: Function) {
+    console.log('Component decorator called');
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+        console.log('Inserting the component in the DOM');
+    }
 }
+
+@Component
+class ProfileComponent { }
