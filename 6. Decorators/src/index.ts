761cf6d939f86f8767f9d6b53f3d2c61756800c0
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-09 22:13:28 +0200

    What are Decorators?

---
+++ 6. Decorators/src/index.ts
@@ -0,0 +1,5 @@
+// What are Decorators?
+// Decorators - are attributes that we apply to our classes and their members and with this we can change how they behave. They are frequently used in Angular and vue Applications.
+class ProfileComponent {
+
+}
