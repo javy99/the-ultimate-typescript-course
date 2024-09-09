7193e3c7c78f95fa3905d7979ee4984a95ed6f4a
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-09 22:43:14 +0200

    Accessor Decorators

--- 6. Decorators/src/index.ts
+++ 6. Decorators/src/index.ts
@@ -71,3 +71,24 @@ class Person5 {
 
 let person5 = new Person5();
 person5.say('Hello');
+
+// Accessor Decorators
+function Capitalize(target: any, propertyName: string, descriptor: PropertyDescriptor) {
+    const original = descriptor.get;
+    descriptor.get = function () {
+        const result = original?.call(this);
+        return (typeof result === 'string') ? result.toUpperCase() : result;
+    }
+}
+
+class Person6 {
+    constructor(public firstName: string, public lastName: string) { }
+
+    @Capitalize
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+let person6 = new Person6('John', 'Doe');
+console.log(person6.fullName);
