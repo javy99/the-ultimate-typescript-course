bc698f3078bdd37e1f9c6f9be1ee880e5c1f4edf
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-10 11:48:17 +0200

    Using the Effect Hook

--- 9. React with TypeScript/reminders-app/src/App.tsx
+++ 9. React with TypeScript/reminders-app/src/App.tsx
@@ -1,29 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import ReminderList from './components/ReminderList';
 import Reminder from './models/reminder';
-
+import reminderService from './services/reminder';
 
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([
-    { id: 1, title: 'Meeting with John' },
-    { id: 2, title: 'Buy groceries' },
-    { id: 3, title: 'Call mom' },
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
 
-  const addReminder = () => {
-    const newReminder: Reminder = {
-      id: Math.floor(Math.random() * 1000),
-      title: 'New reminder',
-    };
-    setReminders([...reminders, newReminder]);
-  }
+  useEffect(() => {
+    loadReminders();
+  }, [])
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  };
 
   return (
     <div className="App">
       <ReminderList items={reminders} />
-      <button onClick={addReminder}>Add reminder</button>
     </div>
   );
 }
