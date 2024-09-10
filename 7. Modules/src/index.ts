d660cb28fe2d7dc4a62806e46e7c08d4b9ffc812
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-10 10:36:47 +0200

    Default Exports

--- 7. Modules/src/index.ts
+++ 7. Modules/src/index.ts
@@ -1,4 +1,5 @@
 import { Circle5, Square5 } from "./shapes"
+import Store5, { Format } from "./storage"
 
 const circle5 = new Circle5(5)
 const square5 = new Square5(10)
@@ -12,3 +13,9 @@ console.log(square5.width)
 // UMD (Universal Module Definition) is a module format that is used in web development. It is a way to define modules so that they can be imported into other modules using the require function.
 // CommonJS is a module format that is used in Node.js. It is a way to define modules so that they can be imported into other modules using the require function.
 // ES2015/ES6 is a module format that is used in modern web development. It is a way to define modules so that they can be imported into other modules using the import and export keywords.
+
+// Default Exports
+// Default exports are used to export a single value from a module. When importing a default export, you can give it any name you want.
+const store5 = new Store5()
+console.log(store5)
+console.log(Format.Raw)
---
+++ 7. Modules/src/storage.ts
@@ -0,0 +1,9 @@
+export default class Store5 { }
+
+export enum Format {
+    Raw,
+    Compressed,
+}
+
+// class Compressor { }
+// class Encryptor { }
