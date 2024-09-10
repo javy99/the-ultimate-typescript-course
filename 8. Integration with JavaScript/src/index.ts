b8cb2fc4ee9fe29b11bf7717ef81c29147df6f3f
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-10 11:16:10 +0200

    Using Definitely Typed Declaration Files

--- 8. Integration with JavaScript/src/index.ts
+++ 8. Integration with JavaScript/src/index.ts
@@ -1,4 +1,5 @@
 import { calculateTax } from "./tax";
+import * as _ from "lodash";
 
 // Including JS Code in TS Projects
 const tax = calculateTax(100); // 30
@@ -18,3 +19,7 @@ console.log(tax2);
 // tax.d.ts - declaration file
 // Declaration files are used to describe the types of the JS code
 // Using declaration files, we can type information to the TypeScript compiler
+
+// Using Definitely Typed Declaration Files
+// https://github.com/DefinitelyTyped/DefinitelyTyped
+_.clone([1, 2, 3]);
