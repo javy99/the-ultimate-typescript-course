84c14a11c3b3cd4e4deefe606d86420ba01b5013
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-10 11:54:03 +0200

    Handling Events

--- 9. React with TypeScript/reminders-app/src/components/ReminderList.tsx
+++ 9. React with TypeScript/reminders-app/src/components/ReminderList.tsx
@@ -2,12 +2,18 @@ import Reminder from "../models/reminder";
 
 interface ReminderListProps {
     items: Reminder[];
+    onRemoveReminder: (id: number) => void;
 }
 
-function ReminderList({ items }: ReminderListProps) {
+function ReminderList({ items, onRemoveReminder }: ReminderListProps) {
     return (
         <ul className="list-group">
-            {items.map((item) => <li className="list-group-item" key={item.id}>{item.title}</li>)}
+            {items.map((item) =>
+                <li className="list-group-item" key={item.id}>
+                    {item.title}
+                    <button className="btn btn-outline-danger mx-2 rounded-pill" onClick={() => onRemoveReminder(item.id)}>Delete</button>
+                </li>
+            )}
         </ul>
     );
 }
