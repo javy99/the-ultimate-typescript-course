d8e04b35f9ef5425bd41e23b9d3eb4f3cdd31e1e
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-10 11:38:29 +0200

    Using the State Hook

--- 9. React with TypeScript/reminders-app/src/App.tsx
+++ 9. React with TypeScript/reminders-app/src/App.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import ReminderList from './components/ReminderList';
+import Reminder from './models/reminder';
 
-const reminders = [
-  { id: 1, title: 'Meeting with John' },
-  { id: 2, title: 'Buy groceries' },
-  { id: 3, title: 'Call mom' },
-];
+
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id: 1, title: 'Meeting with John' },
+    { id: 2, title: 'Buy groceries' },
+    { id: 3, title: 'Call mom' },
+  ]);
+
+  const addReminder = () => {
+    const newReminder: Reminder = {
+      id: Math.floor(Math.random() * 1000),
+      title: 'New reminder',
+    };
+    setReminders([...reminders, newReminder]);
+  }
+
   return (
     <div className="App">
       <ReminderList items={reminders} />
+      <button onClick={addReminder}>Add reminder</button>
     </div>
   );
 }
