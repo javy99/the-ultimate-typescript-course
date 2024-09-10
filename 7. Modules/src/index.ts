457c375fff82d4ae96659382a19b9baec58bc555
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-10 10:26:51 +0200

    Exporting and Importing

---
+++ 7. Modules/src/index.ts
@@ -0,0 +1,7 @@
+import { Circle5, Square5 } from "./shapes"
+
+const circle5 = new Circle5(5)
+const square5 = new Square5(10)
+
+console.log(circle5.radius)
+console.log(square5.width)
---
+++ 7. Modules/src/shapes.ts
@@ -0,0 +1,7 @@
+export class Circle5 {
+    constructor(public radius: number) { }
+}
+
+export class Square5 {
+    constructor(public width: number) { }
+}
