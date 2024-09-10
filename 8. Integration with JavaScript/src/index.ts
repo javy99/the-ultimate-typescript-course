256dcd8b43fc41dbbbe534acea435d9bac8d2132
Javlonbek Kosimov <68327261+javy99@users.noreply.github.com>
2024-09-10 10:59:22 +0200

    Type Checking JS Code

--- 8. Integration with JavaScript/src/index.ts
+++ 8. Integration with JavaScript/src/index.ts
@@ -1,5 +1,10 @@
 import { calculateTax } from './text';
 
 // Including JS Code in TS Projects
-const tax = calculateTax(100);
+const tax = calculateTax(100); // 30
+
 console.log(tax);
+
+// Type Checking JS Code
+const tax1 = calculateTax() // NaN
+console.log(tax1);
